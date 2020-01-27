a9cdb0270ec3e9270d6509fbabbe30c56815ab66
glebson1988 <glebson1988@yandex.ru>
2020-01-27 16:05:10 +0300

    add first_name && last_name to users and friends list

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
   resources :user_stocks, only: %i[create destroy]
 end
