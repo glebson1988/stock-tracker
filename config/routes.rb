21f3bdace25524b019286c9179de87463e9d998b
glebson1988 <glebson1988@yandex.ru>
2020-01-28 09:55:36 +0300

    add js search submission and response

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_friend', to: 'users#search'
   get'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
   resources :user_stocks, only: %i[create destroy]
