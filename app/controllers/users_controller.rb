672ea5ef944888ed501d7173fcd56aab07e2a1d0
glebson1988 <glebson1988@yandex.ru>
2020-01-28 11:20:42 +0300

    add feature: add and remove friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,15 @@ class UsersController < ApplicationController
 
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "Couldn't find user"
+          flash.now[:alert] = "Could not find user"
           format.js { render partial: 'users/friend_result' }
         end
       end
@@ -28,4 +29,5 @@ class UsersController < ApplicationController
       end
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
   resources :user_stocks, only: %i[create destroy]
+  resources :friendships, only: %i[create destroy]
 end
